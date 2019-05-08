f28a82861c115614fb2cccdb2bc3e4cf353bdee6
Yutaro Takase <y.takase331@gmail.com>
2019-05-08 16:33:41 +0900

    あ

--- src/NotifyAvailableDate/NotifyAvailableDate/NotifyAvailableDate.cs
+++ src/NotifyAvailableDate/NotifyAvailableDate/NotifyAvailableDate.cs
@@ -60,7 +60,7 @@ namespace NotifyAvailableDate
         {
             HttpResponseMessage response = await Login();
 
-            var loopCount = 3;
+            var loopCount = 2;
 
             var bookables = new List<Bookable>();
 
