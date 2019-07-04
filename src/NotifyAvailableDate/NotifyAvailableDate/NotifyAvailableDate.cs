904f03948471c353494d1b1fb99295c2ea4e2840
Yutaro Takase <y-takase@pa-consul.co.jp>
2019-07-04 17:46:10 +0900

    土日も１限は嫌だ

--- src/NotifyAvailableDate/NotifyAvailableDate/NotifyAvailableDate.cs
+++ src/NotifyAvailableDate/NotifyAvailableDate/NotifyAvailableDate.cs
@@ -23,7 +23,7 @@ namespace NotifyAvailableDate
 		private static readonly CookieContainer cookie = new CookieContainer();
 		private static readonly HtmlParser parser = new HtmlParser();
 		private static readonly List<string> holidayColor = new List<string>() { "#FF0000", "#0000FF" };
-		private static readonly List<int> holidayTargetTimeZoneIndex = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
+		private static readonly List<int> holidayTargetTimeZoneIndex = new List<int>() { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
 		private static readonly List<int> targetTimeZoneIndex = new List<int>() { 2 };
 
 		private static IConfigurationRoot Configuration { get; }
